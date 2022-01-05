8ae8749a204e783a77a6b98af5acf6358cb22af1
Elv310 <Elvisa310@gmail.com>
2022-01-04 19:28:32 -0500

    Java basics

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	
+	String firstName = "Elvis";
+	String lastName = "Aibangbee";
+            String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);
+
+
+        String createdName = Main.createFullName(firstName, lastName);
+        System.out.println(createdName);
+
+
+
+
+
     }
+
+
+public static String createFullName(String firstName, String lastName){
+        return firstName + " " + lastName;
 }
+}
+
---
+++ src/com/company/NameGenerator.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class NameGenerator {
+String firstName = "Elvis";
+String lastName = "Aibangbee";
+
+    public static void generateFullName(){
+
+    }
+}
